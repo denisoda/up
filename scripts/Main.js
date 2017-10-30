45dabcfbfff52aa045a3cb50508908eab127129e
Eduard Paprotski <fisheroliver9@gmail.com>
2017-10-30 23:21:10 +0300

    Added changes in Main.js

--- scripts/Main.js
+++ scripts/Main.js
@@ -1,68 +1,43 @@
-
-
-
-//Your Google API https://developers.google.com/maps/documentation/javascript/get-api-key
-
+// Your Google API https://developers.google.com/maps/documentation/javascript/get-api-key
 var Google_Api_Key = 'AIzaSyDClvZpnI5xyB118el7chbFSHHlppmdg6E'
-
-
-
-
+ 
 function PageSpeed_Insights () {
+    var inputUrl = document.getElementById("URL_speed").value;
+    
+    if (!inputUrl || !inputUrl.length) {
+        document.getElementById("Result").innerHTML = "Input the site's URL!";
+        return;
+    }
 
-    var inp_url = document.getElementById("URL_speed");
-
-    var url = 'http://webbuilder.design/urls?=' + inp_url;
-
-    fetch(url, {
+    var gooleApiUrl = 'https://www.googleapis.com/pagespeedonline/v2/runPagespeed?url=' + inputUrl + '&strategy=mobile&key=' + Google_Api_Key;
+    var redirectUrl = 'http://webbuilder.design/urls?=' + inputUrl;
+        
+    fetch(gooleApiUrl, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json'
         },
         mode: 'no-cors',
     }).then(function(response) {
-        console.log(response);
+        var data = response.data,
+            result = document.getElementById("Result");
+        
+        result.innerHTML = 'Information about "' + data.title + '" page';
+        result.innerHTML += "<br> You're site speed is " + data.ruleGroups.SPEED.score + ' from 100';
+        result.innerHTML += ("<br>" + "You're site is mobile friendly");
+        
+        fetch(redirectUrl, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            mode: 'no-cors',
+        }).then(function(response) {
+            console.log(response);
+        }, function(error) {
+            console.log(error.message);
+        });
     }, function(error) {
-        console.log(error.message);
-    });
-
-    var data = JSON.parse(httpGet(inp_url.value));
-    var result = document.getElementById("Result");
-    if (inp_url.value.length == 0) {
-        document.getElementById("Result").innerHTML = "Input the site's URL!";
-    }
-    else {
-        try {
-
-            result.innerHTML = 'Information about "' + data.title + '" page';
-            result.innerHTML += "<br> You're site speed is " + data.ruleGroups.SPEED.score + ' from 100';
-            result.innerHTML += ("<br>" + "You're site is mobile friendly");
-
-
-        } catch (err) {
-            document.getElementById("Result").innerHTML = err.message;
-        }
-    }
+        document.getElementById("Result").innerHTML = "You've put incorrect URL or the site doesn't exist, please put URL in the order 'https://example.com' :)";
+    });   
 }
-
-
-//parse of this data https://developers.google.com/apis-explorer/#p/pagespeedonline/v2/pagespeedonline.pagespeedapi.runpagespeed
-
-function JsonParse(file) {
-
-        var file = JSON.parse(file);
-
-        return JSON.parse(file);
-
-    }
-
-//Api Google
-        function httpGet(theUrl)
-        {
-                var xmlHttp = new XMLHttpRequest();
-                xmlHttp.open( "GET",'https://www.googleapis.com/pagespeedonline/v2/runPagespeed?url=' + theUrl + '&strategy=mobile&key=' + Google_Api_Key, false );
-                xmlHttp.send( null );
-                return xmlHttp.responseText;
-            }
-
-
